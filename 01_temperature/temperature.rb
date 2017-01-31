d7b8f10d35fe753f0599e32292cbaa472360e0bd
Kathryn Williams <ksw4321@gmail.com>
2017-01-31 15:41:33 +0000

    update temperature.rb

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,13 @@
 #write your code here
+
+def ftoc(temp_in_f)
+  temp_in_f = temp_in_f.to_f
+  temp_in_c = (temp_in_f - 32) * 5 / 9
+  temp_in_c
+end
+
+def ctof(temp_in_c)
+  temp_in_c = temp_in_c.to_f
+  temp_in_f = temp_in_c * 9 / 5 + 32
+  temp_in_f
+end
