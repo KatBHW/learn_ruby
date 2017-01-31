c8a6df900006ed883078548da8d748705bca7937
Kathryn Williams <ksw4321@gmail.com>
2017-01-31 16:12:37 +0000

    update calculator.rb without bonus

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,16 @@
-#write your code here
+def add(num1, num2)
+  add_total = num1 + num2
+  add_total
+end
+  
+def subtract(num3, num4)
+  subtract_total =  num3 - num4
+  subtract_total
+end
+
+def sum(x)
+  sum_total = 0
+  x.each { |i| sum_total += i}
+  sum_total
+end
+
